dad5365feaa4bc872fd9978cb39c7bd07050c57f
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-10 22:01:38 -0700

    initial project

---
+++ TripPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripPlanner
+//
+//  Created by Ruhsane Sawut on 5/10/19.
+//  Copyright © 2019 ruhsane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
